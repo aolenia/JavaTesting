e7f3c923c55bd8ba7e705fb7ed4e7a1d93ab12f9
Antti Olenius <antti.olenius@cimcorp.com>
2020-06-03 16:01:05 +0300

    Add unversioned files to gitIgnore and added comment to Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello World");
+        System.out.println("Just double check Hello!");
         int[] array = {9, 8937592, 8437, -12, 463, 0, 123};
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(Sort.sortFromSmallestToBiggest(array)));
